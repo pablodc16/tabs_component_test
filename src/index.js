a87e556350e7c6b6f11581b8bca258e7b45f8db0
Pablo Duran <josepablo50@hotmail.com>
2022-05-02 15:32:51 -0600

    set image for all panels
    set image for all panels

--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ import "./styles/main.scss";
 // images
 import surtsey_volcano from "./assets/imgs/surtsey_volcano.jpg";
 
-const surtseyVolcanoImg = document.getElementById('surtseyVolcanoImg');
-surtseyVolcanoImg.src = surtsey_volcano;
+// set image for all panels
+const surtseyVolcanoImg = Object.values(document.getElementsByClassName('surtseyVolcanoImg'));
+surtseyVolcanoImg.forEach(element => {
+  element.src = surtsey_volcano;
+});
 
 // set Listeners for navegation bar
 const navTabs = Object.values(document.getElementsByClassName("nav-tab"));
