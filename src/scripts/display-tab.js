69b4087130da099af89aac4e2c0254b9aa70b1ad
Pablo Duran <josepablo50@hotmail.com>
2022-05-03 11:05:44 -0600

    finished the component

--- src/scripts/display-tab.js
+++ src/scripts/display-tab.js
@@ -7,9 +7,18 @@ const displayTab = (event) => {
     element.style.display = "none";
   });
 
+  // remove the active class from the nav menu
+  const navTabs = Object.values(document.getElementsByClassName("nav-tab"));
+  navTabs.forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  // set the active class for the selected element in the nav menu
+  event.target.classList.add("active");
+
   // display the panel selected on the nav
   const panel = document.getElementById(`tab-${panelNumb}`);
   panel.style.display = "block";
-}
+};
 
 export default displayTab;
